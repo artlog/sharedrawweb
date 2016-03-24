c19dd03bb8f3a61d6257c761992cfba3fe3fa6b7
philippe lhardy <philippe@pavilionartlogiciel>
2016-03-24 22:40:40 +0100

    minimal README improvment

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ if [[ ! -d  artlog_toolbox ]]
 then
     git clone https://github.com/artlog/artlog_toolbox.git artlog_toolbox
     pushd artlog_toolbox
-    git checkout -b shrdrwb shrdrwb
+    git checkout
     popd
 fi
 artlog_toolbox/deploy.sh
